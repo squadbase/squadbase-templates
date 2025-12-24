3d4d71fa8aa40c7119a060bdc6bf30d7410596c5
Naoto Shibata <shibata@queue-inc.com>
2025-12-24 12:15:01 +0900

    remove use server from lib/readSql.ts

--- templates/google-analytics-starter/lib/readSql.ts
+++ templates/google-analytics-starter/lib/readSql.ts
@@ -1,10 +1,8 @@
-"use server";
-
-import { readFile } from "fs/promises";
+import { readFile } from 'fs/promises';
 
 export const readSql = async (filePath: `sql/${string}`): Promise<string> => {
   try {
-    return await readFile(filePath, "utf-8");
+    return await readFile(filePath, 'utf-8');
   } catch (error) {
     throw new Error(`Failed to read SQL file: ${filePath}`);
   }
