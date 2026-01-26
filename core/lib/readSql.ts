41fc5487653558fd3fa1fc81d54ac54018ce8395
Atsuki Hasegawa <58581243+atsuki44@users.noreply.github.com>
2026-01-26 09:58:28 +0900

    fix: remove readSql and /sql directory from core template

--- core/lib/readSql.ts
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'fs/promises';
-
-export const readSql = async (filePath: `sql/${string}`): Promise<string> => {
-  try {
-    return await readFile(filePath, 'utf-8');
-  } catch (error) {
-    throw new Error(`Failed to read SQL file: ${filePath}`);
-  }
-};
